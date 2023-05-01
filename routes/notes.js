63361dd5479ff01bdd309340d8941eafbbd5b6a0
Quise <darkquise@gmail.com>
2023-05-01 01:18:41 -0400

    added more route code to the notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -4,3 +4,23 @@ const fs = require('fs');
 notes.get('/', (req, res) => {
     fs.readFile('../db/db.json')
 })
+
+notes.post('/', (req, res) => {
+    console.log(req.body);
+
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text
+      };
+  
+      readAndAppend(newNote, '../db/db.json');
+      res.json(`Note added successfully 🚀`);
+    } else {
+      res.error('Error in adding note');
+    }
+})
+
+module.exports = notes;
